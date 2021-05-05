7ff8277317b4b103d80d79451feeb21420b7e317
throughtronik <to.isla@gmail.com>
2021-05-05 19:48:01 +0300

    devise added, templates changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_alert_color_bg(type)
+    colors = { 'notice' => 'alert-success',
+               'alert' => 'alert-danger'
+    }
+
+    return 'alert-secondary' if colors[type].nil?
+
+    colors[type]
+  end
+
+  def user_avatar(user)
+    # TODO: upload user avatars
+    asset_pack_path('media/images/user.png')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
-  has_many :events
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 35 }
-  validates :email, presence: true, length: { maximum: 255 }
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  has_many :events
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Пользователь #{rand(777)}" if self.name.blank?
+  end
 end
