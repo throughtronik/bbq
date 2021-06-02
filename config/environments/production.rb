b27649d8dbae5c3dea906ac4c45e4895c29715ae
throughtronik <to.isla@gmail.com>
2021-06-03 02:53:59 +0300

    prod config fixies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
-  ActionMailer::BASE.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
