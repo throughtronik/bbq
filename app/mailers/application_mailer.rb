8d9139eb701d90fce4e16f11d505f815c4404e06
throughtronik <to.isla@gmail.com>
2021-06-04 03:16:15 +0300

    action_mailer config bugfix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   if Rails.env.production?
     default from: ENV['MAILJET_SENDER']
-  end
+  else
     default from: 'test@test.net'
+  end
   layout 'mailer'
 end
