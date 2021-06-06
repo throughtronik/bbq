30dafd4fca6922798a61b604d3de7cfd3874df9b
throughtronik <to.isla@gmail.com>
2021-06-06 05:36:36 +0300

    changed options for lightbox

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,11 +3,20 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
 
+
 import '../styles/application'
 import 'bootstrap/dist/js/bootstrap'
 import 'lightbox2/dist/js/lightbox'
 import '../scripts/ys_map'
 import "@fortawesome/fontawesome-free/css/all"
+
+import lightbox from 'lightbox2/dist/js/lightbox'
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'albumLabel': "Изображение %1 из %2"
+})
 
 const images = require.context('../images', true)
 
