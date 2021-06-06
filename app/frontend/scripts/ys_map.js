054b2b6876c5c0f0753060c984be57db7e24bc3d
throughtronik <to.isla@gmail.com>
2021-06-06 05:17:36 +0300

    ys script fixed

--- app/frontend/scripts/ys_map.js
+++ app/frontend/scripts/ys_map.js
@@ -2,8 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     ymaps.ready(init);
     var myMap;
 
+    let map_div_included = document.getElementById('map');
+
     function init() {
+        
+        if (!map_div_included){
+            return;
+        };
+
         address = document.getElementById('map').getAttribute('data-address');
+
+        if (address == null) {
+            return
+        };
 
         myMap = new ymaps.Map("map", {
             center: [55.76, 37.64],
