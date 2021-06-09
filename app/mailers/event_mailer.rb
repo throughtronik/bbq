504c8c0ab397851041a8396492a20caff3dfe7de
throughtronik <to.isla@gmail.com>
2021-06-09 22:52:47 +0300

    action_mailer refactir

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,24 +1,23 @@
 class EventMailer < ApplicationMailer
-
   def subscription(event, subscription)
     @email = subscription.user_email
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: I18n.t('event_mailer.subscription.email_subject', event_title: event.title)
+    mail to: event.user.email, subject: default_i18n_subject(event_title: event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: I18n.t('event_mailer.comment.email_subject', event_title: event.title)
+    mail to: email, subject: default_i18n_subject(event_title: event.title)
   end
 
   def photo(event, new_photo, email)
     @event = event
     @new_photo = new_photo
 
-    mail to: email, subject: I18n.t('event_mailer.photo.email_subject', event_title: event.title)
+    mail to: email, subject: default_i18n_subject(event_title: event.title)
   end
 end
