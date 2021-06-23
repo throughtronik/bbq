13e450aea540f18a8e5d7adfe0e16791043abf01
throughtronik <to.isla@gmail.com>
2021-06-24 00:01:29 +0300

    Changed default action mailer URL due to ENV['ACTION_MAILER_DEFAULT_URL']

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'missfriends.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['ACTION_MAILER_DEFAULT_URL'] }
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
