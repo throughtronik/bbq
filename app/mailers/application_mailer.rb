1817a0a153be103fc3bbd5fabb17c376de607ce3
throughtronik <to.isla@gmail.com>
2021-06-24 04:35:13 +0300

    Changed Sender Email Appearence

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['MAILJET_SENDER'] || ENV['DEV_MAIL_SENDER']
+  default from: "#{ENV['MAILJET_SENDER_NAME']} <#{ENV['MAILJET_SENDER']}>" || ENV['DEV_MAIL_SENDER']
   layout 'mailer'
 end
--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,7 +2,7 @@
 Mailjet.configure do |config|
   config.api_key = ENV['MAILJET_USERNAME']
   config.secret_key = ENV['MAILJET_PASSWORD']
-  config.default_from = ENV['MAIJET_SENDER']
+  config.default_from = "#{ENV['MAILJET_SENDER_NAME']} <#{ENV['MAILJET_SENDER']}>"
   # Mailjet API v3.1 is at the moment limited to Send API.
   # We’ve not set the version to it directly since there is no other endpoint in that version.
   # We recommend you create a dedicated instance of the wrapper set with it to send your emails.
