d8745ee193ad901faf68daefd5d2695eb7c08151
throughtronik <to.isla@gmail.com>
2021-07-08 02:11:50 +0300

    Capistrano Redis Implemented

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,10 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "ascending.fun", user: "deploy", roles: %w[app db web]
+server "ascending.fun", user: "deploy", roles: %w[app db web resque_worker]
+
+set :resque_environment_task, true
+set :workers, { "#{fetch(:application)}*" => 1 }
 
 # role-based syntax
 # ==================
@@ -19,8 +22,6 @@ server "ascending.fun", user: "deploy", roles: %w[app db web]
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
@@ -28,8 +29,6 @@ server "ascending.fun", user: "deploy", roles: %w[app db web]
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
-
 
 # Custom SSH Options
 # ==================
